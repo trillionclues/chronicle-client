2b884bb02516e69c7899276fb89bde0d6464c9d6
trillionclues <trillionclues@gmail.com>
2025-04-04 15:18:41 +0100

    moved firebase config items

--- secrets.dart
+++
@@ -1,8 +0,0 @@
-class FirebaseConfig {
-  static const FIREBASE_API_KEY = "AIzaSyDQuN_RHQV-8WFMIrNPyfojW0aKJ8Zt87A";
-  static const FIREBASE_APP_ID = "1:53372143848:android:3000a697fec7cdf9b8615d";
-  static const FIREBASE_MESSAGING_SENDER_ID = "53372143848";
-  static const FIREBASE_PROJECT_ID = "chronicle-86dce";
-  static const FIREBASE_STORAGE_BUCKET = "chronicle-86dce.firebasestorage.app";
-  static const FIREBASE_IOS_BUNDLE_ID = "com.chronicle.chronicle";
-}
