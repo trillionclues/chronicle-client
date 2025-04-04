f2182e2a5e1410d2c92fe4dd9f9ab1691b481479
trillionclues <trillionclues@gmail.com>
2025-04-04 15:14:37 +0100

    moved firebase config items

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:chronicle/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
